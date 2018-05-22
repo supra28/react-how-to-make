901af1547757a7f3df307377df27d907694c3587
supra28 <harshitmehrotra@hotmail.com>
2018-05-22 21:52:07 +0530

    updated editor settings

--- src/components/ResponsiveNavbar/Navbar.js
+++ src/components/ResponsiveNavbar/Navbar.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react';
-import './Navbar.css';
+import React, { Component } from "react";
+import "./Navbar.css";
 
 class Navbar extends Component {
-    state={open:false}
-    navRef = React.createRef()
+  state = { open: false };
 
-    toggleMenu = () => {
-       // this.navRef.classList.toggle('responsive')
-       console.log("tototo")
-        this.setState({open:!this.state.open})
-    }
-    render() {
-        return (
-            <nav className={`topnav ${this.state.open?"responsive":""}`} ref={this.navRef} >
-                <a href="#home" className="active">Home</a>
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
-                <a role="button" className="icon" onClick={this.toggleMenu}>
-                    <i className="fa fa-bars"></i>
-                </a>
-
-            </nav>
-        );
-    }
+  toggleMenu = () => {
+    console.log("tototo");
+    this.setState({ open: !this.state.open });
+  };
+  render() {
+    return (
+      <nav
+        className={`topnav ${this.state.open ? "responsive" : ""}`}
+        ref={this.navRef}
+      >
+        <a href="#home" className="active">
+          Home
+        </a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+        <a role="button" className="icon" onClick={this.toggleMenu}>
+          <i className="fa fa-bars" />
+        </a>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
